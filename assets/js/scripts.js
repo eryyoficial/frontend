4eeaecd307c6eafa27779709f1799b774c1ddedc
Hermínio Da Silva André Francisco <eryyoficial@gmail.com>
2024-09-10 12:35:40 +0100

    Test Website Front

---
+++ assets/js/scripts.js
@@ -0,0 +1,23 @@
+
+// Animações ao rolar a página
+document.addEventListener("DOMContentLoaded", function() {
+    animateOnScroll('#home', 'fade-in');
+    animateOnScroll('#novidades', '');
+    animateOnScroll('#porque-escolher-nos', 'fade-in');
+    animateOnScroll('#sobre-os-perfumes', '');
+});
+
+function animateOnScroll(elementSelector, animationClass) {
+    const element = document.querySelector(elementSelector);
+    if (!element) return;
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                element.classList.add(animationClass);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+    observer.observe(element);
+}
+
